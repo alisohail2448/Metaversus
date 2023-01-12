e003d1fba8d116b233cddf4b9c632f6e458baf83
sohailali_upcred <sohail.ali@upcred.ai>
2023-01-12 11:27:54 +0530

    bugs fixed

--- components/CustomTexts.jsx
+++ components/CustomTexts.jsx
@@ -1,8 +1,7 @@
 'use client';
 
-
-import { motion } from "framer-motion";
-import { textContainer, textVariant2 } from "../utils/motion";
+import { motion } from 'framer-motion';
+import { textContainer, textVariant2 } from '../utils/motion';
 
 export const TypingText = ({ title, textStyles }) => (
   <motion.p
@@ -11,8 +10,8 @@ export const TypingText = ({ title, textStyles }) => (
   >
     {Array.from(title).map((letter, index) => (
       <motion.span variants={textVariant2} key={index}>
-        {letter === " " ? "\u00A0" : letter}
-      </motion.span>
+        {letter === ' ' ? '\u00A0' : letter}
+    </motion.span>
     ))}
   </motion.p>
 );
